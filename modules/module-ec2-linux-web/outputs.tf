99ac17bcccea2c6282b1cc8ca1487967f84f2536
agustinasierralima <you@example.com>
2021-04-13 12:24:36 -0300

    fix deploy only ec2 instance

--- modules/module-ec2-linux-web/outputs.tf
+++ modules/module-ec2-linux-web/outputs.tf
@@ -1,3 +1,9 @@
-output "network_interface_id" {
-  value = aws_network_interface.main_network_interface.id
+output "server_private_ip" {
+  value = aws_instance.ec2-instance.private_ip
+}
+output "server_id" {
+  value = aws_instance.ec2-instance.id
+}
+output "public_ip" {
+  value       = aws_instance.ec2-instance.public_ip
 }
