846308577ef5ba5a04da0a44f611df6482e1dd81
agustina <78379232+agustinasierralima@users.noreply.github.com>
2021-05-18 14:18:56 -0300

    Update outputs.tf

--- modules/module-network-linux-web/outputs.tf
+++ modules/module-network-linux-web/outputs.tf
@@ -1,3 +1,9 @@
 output "network_interface_id" {
   value = aws_network_interface.network_interface.id
 }
+output "vpc_id" {
+  value = aws_vpc.vpc.id
+}
+output "security_group_id" {
+  value = aws_security_group.security_group.id
+}
